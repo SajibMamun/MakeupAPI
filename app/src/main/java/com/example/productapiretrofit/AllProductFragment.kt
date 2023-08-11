83422bc23bcea06df0c2653b1f51e2469d31d284
Sajib Mamun <sajibmamun138@gmail.com>
2023-08-12 03:13:12 +0600

    Coroutine Live data with observer

--- app/src/main/java/com/example/productapiretrofit/AllProductFragment.kt
+++ app/src/main/java/com/example/productapiretrofit/AllProductFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import com.example.productapiretrofit.API.ProductService
 import com.example.productapiretrofit.API.RetrofitClient
@@ -14,6 +15,9 @@ import com.example.productapiretrofit.databinding.FragmentAllProductBinding
 import com.example.productapiretrofit.dataclass.ResponseProduct
 import com.example.productapiretrofit.dataclass.ResponseProductItem
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -26,6 +30,8 @@ class AllProductFragment: Fragment(),ProductAdapter.ProductListener {
     lateinit var adapter: ProductAdapter
     @Inject
     lateinit var service: ProductService
+
+    var productlist:MutableLiveData<ResponseProduct> = MutableLiveData()
 
 
     override fun onCreateView(
@@ -44,11 +50,20 @@ class AllProductFragment: Fragment(),ProductAdapter.ProductListener {
 
 
 
+CoroutineScope(Dispatchers.IO).launch {
+    var response=service.getAllProducts()
 
+    if(response.code()==200)
+    {
+        productlist.postValue(response.body())
+    }
 
+}
 
+productlist.observe(viewLifecycleOwner){
+    adapter.submitList(it)
+}
 
-        var CallApiService=service.getAllProducts()
 
 
 
